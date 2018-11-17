8f5c52b9fbab8aaae854c49c7bbd14678ad0a181
ringius <45101382+ringius@users.noreply.github.com>
2018-11-17 09:24:29 +0100

    first version

---
+++ SevenW/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SevenW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player pelle = new Player(@"Pelle");
+            Player olle = new Player("Olle");
+            Player kalle = new Player("Kalle");
+
+            pelle.EastPlayer = olle;
+            pelle.WestPlayer = kalle;
+            
+            Console.WriteLine(pelle.ToString());
+            Console.ReadKey();
+        }
+    }
+}
